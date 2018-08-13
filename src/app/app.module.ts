d521133a1e6ca84b28bc54f0b61820e7b0f847c9
Raj Rathore <Raj.Rathore@tatacommunications.com>
2018-08-13 09:57:06 +0530

    Made all the shopping list features as a separate module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,15 @@ import {RecipesComponent} from './recipes/recipes.component';
 import {RecipeListComponent} from './recipes/recipe-list/recipe-list.component';
 import {RecipeItemComponent} from './recipes/recipe-list/recipe-item.component';
 import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
-import {ShoppingListComponent} from './shopping-list/shopping-list.component';
-import {ShoppingListAddComponent} from './shopping-list/shopping-list-add.component';
 import {DropdownDirective} from './dropdown.directive';
 import {RecipeService} from './recipes/recipe.service';
 import {ShoppingListService} from './shopping-list/shopping-list.service';
 import {routing} from './app.routes';
 import {RecipeStartComponent} from './recipes/recipe-start.component';
 import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {ReactiveFormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
+import {ShoppingListModule} from "./shopping-list/shopping-list.module";
 
 
 @NgModule({
@@ -28,14 +27,12 @@ import {HttpModule} from "@angular/http";
     RecipeListComponent,
     RecipeItemComponent,
     RecipeDetailComponent,
-    ShoppingListComponent,
-    ShoppingListAddComponent,
     DropdownDirective,
     RecipeStartComponent,
     RecipeEditComponent
   ],
   imports: [
-    BrowserModule, routing, FormsModule, ReactiveFormsModule, HttpModule
+    BrowserModule, routing, ReactiveFormsModule, HttpModule, ShoppingListModule
   ],
   providers: [RecipeService, ShoppingListService],
   bootstrap: [AppComponent]
